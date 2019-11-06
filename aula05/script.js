f25a7dd4ae44fefd7bd3f8d8000d2792ed0040a4
unknown <betopaulohenrique@gmail.com>
2019-11-05 21:58:26 -0300

    Configurando login Java

---
+++ aula05/script.js
@@ -0,0 +1,15 @@
+let loginDeterminado = "betopaulo";
+let senhaDeterminada = "tricolo";
+var cont = ""
+
+let login = prompt("Digite seu login : ");
+
+if(login == loginDeterminado){
+     var senha  = prompt("Digite a sua senha : ");
+     alert("Usuário logado  : ");
+} else{
+    alert("Usuário não encontrado : ");
+}
+
+
+
