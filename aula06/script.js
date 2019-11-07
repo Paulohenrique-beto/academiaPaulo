f3ced87a152587a573526c47186b3b9f642b5a37
unknown <betopaulohenrique@gmail.com>
2019-11-06 21:04:33 -0300

    Aula 06

---
+++ aula06/script.js
@@ -0,0 +1,20 @@
+// let heroes = ["FLash","Aquaman","Capitao America","Hulk","Homem de ferro"];
+// let i = 0;
+
+
+// while(i < heroes.length){
+//     console.log(heroes[i]);
+//     i++;
+    
+// }
+
+
+// let heroes = ["FLash","Aquaman","Capitao America","Hulk","Homem de ferro"];
+
+// for(let e  = 0; e < heroes.length; e++){
+//     console.log(heroes[e]);
+// }
+
+// let cores = ["amarelo","vermelho", "azul"]
+
+// console.log(cores.length)
