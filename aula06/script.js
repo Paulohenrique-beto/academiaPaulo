0fcd43e6ec01a9ee26e9105d43f5b30f182daef6
unknown <betopaulohenrique@gmail.com>
2019-11-07 20:03:49 -0300

    atualização de exercicio da aula

--- aula06/script.js
+++ aula06/script.js
@@ -18,3 +18,26 @@
 // let cores = ["amarelo","vermelho", "azul"]
 
 // console.log(cores.length)
+
+
+
+
+// Exercicio da escada 
+
+// Construção de variáveis
+
+let simbolo = prompt("Digite seu simbolo")
+let quantidade = prompt("Digite a quantidade de degrais")
+let auxiliar = simbolo
+
+// Construção do for 
+// Para construir o for foi necessário criar a variável do index dizendo que ele começa no numero 0 
+// quando o index for menor qua a variável quantidade ; acrescenta +1
+
+for(let i = 0 ; i<quantidade;i++){
+    if(i==0){
+        console.log(simbolo)
+    }else{
+        console.log(simbolo+=auxiliar)
+    }
+}
