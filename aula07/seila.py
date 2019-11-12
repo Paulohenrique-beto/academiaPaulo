dbafb6b3d5fd06fe8bd9927e27bb4f6a94a2df05
unknown <betopaulohenrique@gmail.com>
2019-11-11 21:56:16 -0300

    Aula de Python

---
+++ aula07/seila.py
@@ -0,0 +1,39 @@
+# nome = input("Digite seu nome : ")
+# anos = int(input("Quantos anos você tem ? "))
+# robie = str(input("Qual o seu Robie : "))
+
+# if anos == 22:
+#     print("Sua idade esta correta")
+
+# elif anos  > 22:
+#     print("Sua idade nâo é esta")
+
+
+# else :
+#     print("Sua idade não esta correta")
+
+
+
+numero = 0 
+
+while numero < 5:
+    print(numero)
+
+    numero = numero + 1 
+
+
+lista = ["cubo magico", "Biblia", "pastor", "surf","manga_com_leite"]
+
+# for item in lista:
+#     print(item)
+
+# for i in range(0, len(lista)):
+#     print(lista[i])
+
+
+# for i in range(0, len(lista),2):
+#      print(lista[i])
+
+
+for i in range(len(lista)):
+     print(lista[i])
